76485e72d843ce71c1172727f7fd6a40f02b3074
benardcheruiyot <bcheruiyot221@gmail.com>
2022-10-05 11:37:58 +0300

    adding newpost filr to component folder

---
+++ components/NavBar.js
@@ -0,0 +1,54 @@
+
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import Login from "./Login";
+
+
+function NavBar() {
+
+    const [showForm, setShowForm]= useState(false)
+    function handleClick(){
+        setShowForm(showForm=>!showForm)
+    }
+    return (
+      <div className="nb">
+      <ul >
+      
+      <li><NavLink to="/about" style={({ isActive }) => ({
+              color: isActive ? '#fff' : '#545e6f',
+              background: isActive ? '#7600dc' : '#f0f0f0',
+              textDecoration: 'none',
+              borderRadius: '40px',
+              padding: '4px'
+            })} > About Us </NavLink></li>
+      <li><NavLink to="/contact" style={({ isActive }) => ({
+              color: isActive ? '#fff' : '#545e6f',
+              background: isActive ? '#7600dc' : '#f0f0f0',
+              textDecoration: 'none',
+              borderRadius: '40px',
+              padding: '4px'
+            })} > Contact Us</NavLink></li>
+      <li><NavLink to="/" style={({ isActive }) => ({
+              color: isActive ? '#fff' : '#545e6f',
+              background: isActive ? '#7600dc' : '#f0f0f0',
+              textDecoration: 'none',
+              borderRadius: '40px',
+              padding: '3px'
+            })} >Home </NavLink></li>
+     
+
+      <h3>DAILYNEWS</h3>
+      <div>
+       <button onClick={() => setShowForm(prev => !prev)}>Login</button>
+        
+        {showForm && <Login/>}
+        
+        </div>
+        
+        </ul>
+      </div>
+    );
+  }
+  
+  export default NavBar;
---
+++ components/NewPost.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Home from "./Home";
+
+
+function NewPost({onSubmission}){
+    const[newPostForm, setNewPostForm]= useState({title:"", content:"",author:""})
+    
+
+
+
+;
+
+    function handleOnChange(event){
+        event.preventDefault()
+        setNewPostForm(newPostForm=>({...newPostForm,[event.target.name]: event.target.value}))
+        }
+
+        function handleSubmit(event){
+            event.preventDefault()
+            onSubmission(newPostForm)
+            setNewPostForm({title:"",content:"", author:""} )
+           return <Home />
+            
+        }
+
+return(
+    <div>
+
+    <form onSubmit={handleSubmit} className="fm">
+    Title:<br></br>
+<input onChange={handleOnChange} name="title" value={newPostForm.title} placeholder="Topic" /><br></br>
+Username:<br></br>
+<input onChange={handleOnChange} name="author" value={newPostForm.author} placeholder="your username" /><br></br>
+Content:<br></br>
+<textarea  onChange={handleOnChange} name="content" value={newPostForm.content} placeholder="Type the news..." rows={10} /><br></br>
+<input type="submit" value="Publish" />
+
+</form>
+
+</div>
+)
+
+
+    }
+    export default NewPost;
+    
