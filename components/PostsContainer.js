b45adc0917a7b09d1258de7e46fbedc67efbc056
benardcheruiyot <bcheruiyot221@gmail.com>
2022-10-05 11:45:37 +0300

    adding postcontainer file to component folder

---
+++ components/PostsContainer.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import Post from "./Post";
+import { useState } from "react";
+import PostDelete from "./PostDelete";
+
+
+
+function PostsContainer(){
+
+const [posts, setPosts]= useState([])
+
+useEffect(() => {
+  fetch("https://my-server1121.herokuapp.com/posts" )
+    .then((r) => r.json())
+    .then((posts) => setPosts(posts));
+}, []);
+
+
+  function handleDeleteClick(id) {
+if (window.confirm("Do you want to delete this post?")){
+posts.splice(id,1);
+}
+setPosts([...posts])
+  }
+    return(
+  
+
+<div>
+
+{posts.map((posts)=>(<Post key={posts.id} title={posts.title} content={posts.content} author = {posts.author} >
+  <button>remove</button>
+  </Post>)) }
+  <div>
+  <PostDelete>
+                                 key={posts.id}
+                                posts={posts}
+                                onDeleteClick={handleDeleteClick}
+         
+                              </PostDelete>
+                              
+  </div>
+  </div>
+
+
+)
+
+}
+export default PostsContainer;
